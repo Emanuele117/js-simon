c7b5ec28248290ea8dd8af4c3c4064f2ce735150
Emanuele117 <emanuele.pica7@gmail.com>
2021-10-27 15:24:43 +0200

    base html

---
+++ main.js
@@ -0,0 +1,6 @@
+//un alert espone 5 numeri generati casualmente , da lì parte un timer di 30 secondi.
+
+// dopo 30 secondi l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
+
+//Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+
